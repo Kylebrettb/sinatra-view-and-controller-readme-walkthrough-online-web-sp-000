7fb249ab666f41255eb71740d5197344ca1b4273
Kyle  Beckstead <kyle21beckstead@gmail.com>
2019-04-12 18:19:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,26 +5,6 @@ class App < Sinatra::Base
     erb :reverse
   end
 
-  post '/reverse' do
-    erb :reversed
-  end
-
-  get '/friends' do
-    # Write your code here!
-
-  end
-end
-
-
-
-
-require_relative 'config/environment'
-
-class App < Sinatra::Base
-  get '/reverse' do
-    erb :reverse
-  end
-
 post '/reverse' do
   original_string = params["string"]
   @reversed_string = original_string.reverse
